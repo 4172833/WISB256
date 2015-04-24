b2b6b6e3030248b6ab0cef9d8ec72e9004017e60
a4172833 <g.h.m.boot@students.uu.nl>
2015-04-24 13:41:22 +0000

    Opdracht 2.

--- Opdracht1/ManyGreetings.py
+++ Opdracht1/ManyGreetings.py
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-naam = input("Name? ")
-print("Hello, "+naam+"!")
-=======
-naam = input("What is your name? ")
-print("Hello "+naam)
-
 naam = input("Name? ")
 aantal = input("How many greetings? ")
 
 print(aantal+ " x Hello, "+naam+"!")
->>>>>>> 2910d425a2c225a6a868e9cfebb0675a4c1cd719
