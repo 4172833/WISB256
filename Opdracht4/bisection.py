8c3c2ddf73aed1c8c43ade4ed5abaa28756f044e
a4172833 <g.h.m.boot@students.uu.nl>
2015-05-21 14:19:36 +0000

    Aangepast voor a=0, b=0, m=0.

--- Opdracht4/bisection.py
+++ Opdracht4/bisection.py
@@ -1,9 +1,15 @@
 def findRoot(f,a,b,epsilon):
     while True:
-        m = float((a+b)/2)
-        if abs(float(a-b)) <= epsilon:
+        m = (a+b)/2
+        if abs(a-b) <= epsilon:
             return(m)
         elif (f(m) < 0 and f(a) > 0) or (f(m) > 0 and f(a) < 0):
             b = m
         elif (f(m) > 0 and f(b) < 0) or (f(m) < 0 and f(b) > 0):
             a = m
+        elif f(m) == 0:
+            return(m)
+        elif f(a) == 0:
+            return(a)
+        elif f(b) == 0:
+            return(b)
